c12bbf7cf8a1eb016287ef3873faa943f642c116
abbasalit987 <abbasalit987@gmail.com>
2023-04-14 21:59:14 +0530

    commit 5/ added backend code with express js

--- frontend/main.js
+++ frontend/main.js
@@ -6,45 +6,46 @@ let loginForm = document.getElementById("loginForm")
 
 const getData = async () => {
 	try {
-        const res = await axios.get(`${link}/getstockinfo`)
-        for (let i =0; i<res.data.length; i++){
-            addUser(res.data[i])
+        const res = await axios.get(`${link}/stockinfo`)
+        console.log(res.data);
+        for (let i =0; i<res.data.allItems.length; i++){
+            addItem(res.data.allItems[i])
         }
     } catch (error) {
         console.log(error)
     }
 };
 
-const postData = async (user_details) => {
+const postData = async (item_details) => {
 	try{
-        const res = await axios.post(`${link}/stockinfo`, user_details)
-        addUser(res.data)
+        const res = await axios.post(`${link}/stockinfo`, item_details)
+        addItem(res.data.newItemDetail)
     } catch(error) {
         console.log(error)
     }
 };
 
-const putData = async (user_details_s, user_details_c) => {
+const putData = async (item_details_s, item_details_c) => {
     try {
-        const res = await axios.put(`${link}/stockinfo/${user_details_s._id}`, user_details_c)
-        addUser(user_details_s)
+        const res = await axios.put(`${link}/stockinfo/${item_details_s.id}`, item_details_c);
+        addItem(item_details_s)
     } catch(error){
         console.log(error)
     }
 };
 
-const onClick = async (num,user_details) => {
+const onClick = async (num,item_details) => {
     try {
-        let user_details_update = {
-            Candy: user_details.Candy,
-            Description : user_details.Description,
-            Price : user_details.Price,
-            Quantity: user_details.Quantity - num
+        let item_details_update = {
+            item_name: item_details.item_name,
+            item_description : item_details.item_description,
+            price : item_details.price,
+            quantity: item_details.quantity - num
         }
 
-        user_details.Quantity = user_details.Quantity - num
+        item_details.quantity = item_details.quantity - num
 
-        putData(user_details, user_details_update)
+        putData(item_details, item_details_update)
         
     } catch(error) {
         console.log(error)
@@ -55,8 +56,6 @@ window.addEventListener("DOMContentLoaded", () => {
     getData()
 })
 
-
-
 function save(event) {
     
     event.preventDefault()
@@ -66,28 +65,28 @@ function save(event) {
     price = event.target.field3.value
     quantity = event.target.field4.value
 
-    let user_details = {
+    let item_details = {
         Candy: candyName,
         Description : description,
         Price : price,
         Quantity: quantity
     };
 
-    postData(user_details)
+    postData(item_details)
   }
 
-function addUser(user_details) {
+function addItem(item_details) {
     
-    const parentElem = document.getElementById('users')
+    const parentElem = document.getElementById('items')
     const childElem = document.createElement('li')
-    childElem.textContent = user_details.Candy + ' - ' + user_details.Description + ' - ' + user_details.Price + ' - ' + user_details.Quantity
+    childElem.textContent = item_details.item_name + ' - ' + item_details.item_description + ' - ' + item_details.price + ' - ' + item_details.quantity;
 
     const Buy1 = document.createElement('input')
     Buy1.type = 'button'
     Buy1.value = 'Buy 1'
     Buy1.onclick = () => {
         parentElem.removeChild(childElem)
-        onClick(1, user_details) 
+        onClick(1, item_details) 
     }
 
     const Buy2 = document.createElement('input')
@@ -95,7 +94,7 @@ function addUser(user_details) {
     Buy2.value = 'Buy 2'
     Buy2.onclick = () => {
         parentElem.removeChild(childElem)
-        onClick(2, user_details)
+        onClick(2, item_details)
     }
 
     const Buy3 = document.createElement('input')
@@ -103,7 +102,7 @@ function addUser(user_details) {
     Buy3.value = 'Buy 3'
     Buy3.onclick = () => {
         parentElem.removeChild(childElem)
-        onClick(3, user_details)
+        onClick(3, item_details)
     }
     
     childElem.appendChild(Buy1)
